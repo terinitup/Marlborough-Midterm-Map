a7fca059668f0ed7a11d3753479c9653f8b87577
terinitup <terinlee@gmail.com>
2026-09-23 14:30:08 -0700

    adding new map

--- sketch.js
+++ sketch.js
@@ -21,9 +21,10 @@ let data = {
 
 
 async function setup() {
-	img  = await loadImage('Usa.svg.webp');
+	img  = await loadImage('USOutline.svg');
+	imageMode(CENTER)
 	
-  createCanvas(1200, 700);
+  createCanvas(windowWidth, windowHeight);
  
   TopY=height/6;
   BottomY=random(height / 2, height - 50);
@@ -43,10 +44,10 @@ async function setup() {
 }
 
 function draw() {
-  background(0);
+  background(255);
   
   if (img)
-	image(img, 0, 0);
+	image(img, width/2, height/2);
   
    for (let s of squares){
 	s.shift();
